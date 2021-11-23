509fa198833697a40f8ec0001d4a25c2941aebc0
Mert Erciyas <99058235@mydavinci.nl>
2021-11-23 11:54:47 +0100

    Ice Salon Remasterd

---
+++ Opdrachten/Papi_Gelato_Remasterd.py
@@ -0,0 +1,37 @@
+# 99058235 Mert Erciyas
+import time,os,sys
+
+def clearScreen(sleepTime):
+    time.sleep(sleepTime)
+    os.system("cls")
+
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        #time.sleep(0.05)
+
+def iceCream():
+    choice = ""
+    coneOrCup = ""
+    print_slow("Welcome in Papi Gelato you can choose any taste of ice cream you want.\n")
+    flavour = input("What flavour would you like with that?\n ⚫ Strawberry.\n ⚫ Chocolate.\n ⚫ Mint.\n ⚫ Vanille.\n").lower()
+    amount = int(input("How many ice cream balls would you like?\n"))
+    if amount <= 3:
+        coneOrCup = input(f"Do you want your {amount} {flavour} ice cream balls in a cone or cup?\n")
+    elif amount <= 8:
+        print_slow(f"You will get a cup with {amount} {flavour} ice cream balls inside.\n")
+    elif amount > 8:
+        print_slow("Sorry, we dont have bigger cups.\n") 
+        clearScreen(1)
+        iceCream()
+    choice = input(f"Here is your {coneOrCup} with {amount} {flavour} ice cream balls. Do you want to order again? (Y/N)\n").lower()
+    if choice == "y":
+        clearScreen(1)
+        iceCream()
+    elif choice == "n":
+        print_slow("Thank you and see you again!\n")
+    else:
+        print_slow("Sorry i didnt understand that.\n")
+
+iceCream()
