e45bbc56e3dd3b6c50481d867c534e5b800b1a48
Mert Erciyas <99058235@mydavinci.nl>
2021-11-25 13:37:55 +0100

    Papi Gelato Remasterd Remasterd

--- Opdrachten/Papi_Gelato_Remasterd_Remasterd.py
+++ Opdrachten/Papi_Gelato_Remasterd_Remasterd.py
@@ -11,10 +11,15 @@ def print_slow(str):
         sys.stdout.flush()
         #time.sleep(0.05)
 
+A = False
+B = False
+C = False
+D = False
+
+toppingPrice = 0
 bakjePrijs = 0.75
 cone = 0
 cup = 0
-
 bolletjesPrijs = 1.10
 horrentjesPrijs = 1.25
                                            
@@ -24,9 +29,23 @@ def iceCream():
     global cone
     global amount
     global cup
+    global toppingPrice
+    global toppings
     print_slow("Welcome in Papi Gelato.\n")
     flavour = input("What flavour would you like with that?\n ⚫ Strawberry.\n ⚫ Chocolate.\n ⚫ Mint.\n ⚫ Vanille.\n").lower()
+    clearScreen(1)
     amount = int(input("How many ice cream balls would you like?\n"))
+    clearScreen(1)
+    toppings = input("What kind of toppings would you like?\n A. Nothing\n B. Cream\n C. Sprinkels\n D. Carmel\n").lower()
+    clearScreen(1)
+    if toppings == "a":
+        A = True
+    elif toppings == "b":
+        B = True
+    elif toppings == "c":
+        C = True
+    elif toppings == "d":
+        D = True
     if amount <= 3:
         coneOrCup = input(f"Do you want your {amount} {flavour} ice cream balls in a cone or cup?\n")
     elif amount <= 8:
@@ -36,16 +55,27 @@ def iceCream():
         print_slow("Sorry, we dont have bigger cups.\n") 
         clearScreen(1)
         iceCream()
-    bolletjesKosten = amount * bolletjesPrijs
+    bolletjesKosten = round(amount * bolletjesPrijs,2)
     if coneOrCup == "cone":
         cone = 1
     elif coneOrCup == "cup":
         cup = 1
     bakjeKosten = bakjePrijs * cup
     horrentjesKosten = horrentjesPrijs * cone
-    total = horrentjesKosten + bakjeKosten + bolletjesKosten
-    print(f"\n  --------[Papi Gelato]-------- \n\n Balls   {amount} X {bolletjesPrijs} = {bolletjesKosten}\n Cones   {cone} X {horrentjesPrijs} = {horrentjesKosten}\n Cup     {cup} X {bakjePrijs} = {bakjeKosten}\n-----------------------------\n Total              {total}\n")
-    choice = input(f"Here is your {coneOrCup} with {amount} {flavour} ice cream balls. Do you want to order again? (Y/N)\n").lower()
+    if toppings == "a":
+        toppingPrice = 0
+    elif toppings == "b":
+        toppingPrice = 0.50
+    elif toppings == "c":
+        toppingPrice = 0.30 * amount
+    elif toppings == "d":
+        if cone == 1:
+            toppingPrice = 0.60
+        else:
+            toppingPrice = 0.90
+    total = horrentjesKosten + bakjeKosten + bolletjesKosten + toppingPrice
+    print(f"\n  --------[Papi Gelato]-------- \n\n Balls   {amount} X {format(bolletjesPrijs,'.2f')} = {format(bolletjesKosten,'.2f')}\n Cones   {cone} X {format(horrentjesPrijs,'.2f')} = {format(horrentjesKosten,'.2f')}\n Cup     {cup} X {format(bakjePrijs,'.2f')} = {format(bakjeKosten,'.2f')}\n Topping 1 X {format(toppingPrice,'.2f')}  = {format(toppingPrice,'.2f')}\n-----------------------------\n Total              {format(total,'.2f')}\n")
+    choice = input(f"Here is your order with {amount} {flavour} ice cream balls. Do you want to order again? (Y/N)\n").lower()
     if choice == "y":
         clearScreen(1)
         iceCream()
